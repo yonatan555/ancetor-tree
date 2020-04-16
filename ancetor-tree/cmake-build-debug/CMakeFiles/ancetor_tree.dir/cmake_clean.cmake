33aa59b637c3225408ed440c5568d6af751a0e68
yonatan555 <57701623+yonatan555@users.noreply.github.com>
2020-04-16 17:45:13 +0300

    Add files via upload

---
+++ ancetor-tree/cmake-build-debug/CMakeFiles/ancetor_tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ancetor_tree.dir/main.cpp.o"
+  "ancetor_tree"
+  "ancetor_tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ancetor_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
